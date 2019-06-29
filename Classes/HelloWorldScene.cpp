00934194027e22a90c4217b2fa8d70159af618b1
uedaryota <mr.335186@gmail.com>
2019-06-29 09:19:08 +0900

    何もない状態に

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -101,36 +101,36 @@ bool HelloWorld::init()
         this->addChild(label, 1);
     }
 
-	for (int i = 0; i < 10; i++)
-	{
-		//float sx, sy;
-		//sx = (float)rand() / RAND_MAX * visibleSize.width;
-		//sy = (float)rand() / RAND_MAX * visibleSize.height;
-		float sx, sy;
-		sx = (float)rand() / RAND_MAX * visibleSize.width;
-		sy = (float)rand() / RAND_MAX * visibleSize.height;
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	//float sx, sy;
+	//	//sx = (float)rand() / RAND_MAX * visibleSize.width;
+	//	//sy = (float)rand() / RAND_MAX * visibleSize.height;
+	//	float sx, sy;
+	//	sx = (float)rand() / RAND_MAX * visibleSize.width;
+	//	sy = (float)rand() / RAND_MAX * visibleSize.height;
 
-		sprite[i] = Sprite::create("neko.png");
-		this->addChild(sprite[i]);
-		sprite[i]->setPosition(Vec2(100 * i, visibleSize.height / 2.0f));
-		//sprite[i]->setPosition(Vec2(sx, sy));
-		//sprite[i]->setPosition(Vec2(100 * i, visibleSize.height/2.0f));
-		sprite[i]->setPosition(Vec2(sx, sy));
-		sprite[i]->setScale(0.1f);
+	//	sprite[i] = Sprite::create("neko.png");
+	//	this->addChild(sprite[i]);
+	//	sprite[i]->setPosition(Vec2(100 * i, visibleSize.height / 2.0f));
+	//	//sprite[i]->setPosition(Vec2(sx, sy));
+	//	//sprite[i]->setPosition(Vec2(100 * i, visibleSize.height/2.0f));
+	//	sprite[i]->setPosition(Vec2(sx, sy));
+	//	sprite[i]->setScale(0.1f);
 
-		//float ex, ey;
-		//ex = (float)rand()/ RAND_MAX * visibleSize.width;
-		//ey = (float)rand()/ RAND_MAX * visibleSize.height;
-		float ex, ey;
-		ex = (float)rand() / RAND_MAX * visibleSize.width;
-		ey = (float)rand() / RAND_MAX * visibleSize.height;
+	//	//float ex, ey;
+	//	//ex = (float)rand()/ RAND_MAX * visibleSize.width;
+	//	//ey = (float)rand()/ RAND_MAX * visibleSize.height;
+	//	float ex, ey;
+	//	ex = (float)rand() / RAND_MAX * visibleSize.width;
+	//	ey = (float)rand() / RAND_MAX * visibleSize.height;
 
-		//MoveTo* action1 = MoveTo::create(1.0f, Vec2(ex, ey));
-		JumpBy* action1 = JumpBy::create(1.0f, Vec2(300, 0), 300, 1);
-		MoveTo* action2 = MoveTo::create(1.0f, Vec2(ex, ey));
-		//JumpBy* action1 = JumpBy::create(1.0f, Vec2(300,0), 300, 1);
-		sprite[i]->runAction(action1);
-	}
+	//	//MoveTo* action1 = MoveTo::create(1.0f, Vec2(ex, ey));
+	//	JumpBy* action1 = JumpBy::create(1.0f, Vec2(300, 0), 300, 1);
+	//	MoveTo* action2 = MoveTo::create(1.0f, Vec2(ex, ey));
+	//	//JumpBy* action1 = JumpBy::create(1.0f, Vec2(300,0), 300, 1);
+	//	sprite[i]->runAction(action1);
+	//}
 
 	//—”‚Ì‰Šú‰» C#¨(Random r = new Random();)
 	//srand(time(nullptr));
